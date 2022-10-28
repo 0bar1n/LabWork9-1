6dc51aeba990e4f6c451285ce013c4c7687ac160
barinskaya <1388zxc@gmail.com>
2022-10-28 21:22:00 +0300

    Добавьте файлы проекта.

---
+++ LabWork9-1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LabWork9_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 101; i++)//выполнение цикла, пока верно условие
+            {
+                if (i % 2 != 0)//проверка выполнения условия нечетности чисел
+                {
+                    Console.Write("{0} ", i);//вывод на консоль
+                }
+            }
+            Console.ReadKey(); //ожидание нажатия клавиши
+            return; //возврат цикла
+        }
+    }
+}
